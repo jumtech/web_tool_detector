076697518da371929c94df6fe8d30a94f9966ab2
jumtech <despairing.clock+jumGithub@gmail.com>
2017-08-20 17:13:29 +0900

    add minimum extension for devtools with Vue.js

---
+++ src/devtools.js
@@ -0,0 +1,6 @@
+chrome.devtools.panels.create(  
+  "WebTools",
+  "",
+  "./panel.html",
+  (panel) => {}
+);
---
+++ src/panel.js
@@ -0,0 +1,8 @@
+new Vue({
+  el: "#container",
+  data: {
+    hoge: "hoge"
+  },
+  created: () => { /* 初期処理 */ },
+  methods: { /* メソッド */ }
+});
