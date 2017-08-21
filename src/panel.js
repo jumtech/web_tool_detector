e9a7c79db9d91dcbb0b6b1bf019e89f1cd680bfb
jumtech <despairing.clock+jumGithub@gmail.com>
2017-08-22 08:00:34 +0900

    make ga red when found

--- src/panel.js
+++ src/panel.js
@@ -1,21 +1,37 @@
 new Vue({
   el: "#container",
   data: {
-    msg: ""
+    tools: []
   },
   created: function() {
+    const WEB_TOOLS = _get_web_tools();
+    WEB_TOOLS.forEach((tool) => {
+      this.tools.push(tool);
+    });
     // 各リクエスト完了時
     chrome.devtools.network.onRequestFinished.addListener((req) => {
       const url = req.request.url;
-      if (url.indexOf("google-analytics.com") !== -1) {
-        this.msg = "このサイトから、GoogleAnalyticsの香りがするでゲソ";
-      }
+      this.tools.forEach((tool, i) => {
+        if (url.indexOf(tool.domain) !== -1) {
+          const el = document.getElementById(tool.id);
+          el.classList.add("found");
+        }
+      });
     });
     // 画面更新/遷移時
     chrome.devtools.network.onNavigated.addListener((url) => {
-        this.msg = "";
       }
     );
   },
   methods: { /* メソッド */ }
 });
+
+function _get_web_tools() {
+  return [
+    {
+      id: "ga",
+      name: "Google Analytics",
+      domain: "google-analytics.com",
+    },
+  ]
+}
