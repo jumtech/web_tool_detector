f0bd3f0478d21e1d978b99db38bc3df4f90adbf6
jumtech <despairing.clock+jumGithub@gmail.com>
2017-08-23 00:27:12 +0900

    fix style and add some info about tools

--- src/panel.js
+++ src/panel.js
@@ -32,84 +32,118 @@ new Vue({
 function _get_web_tools() {
   return [
     {
+      name: "Google Tag Manager",
+      id: "gtm",
+      domain: "googletagmanager.com",
+      url: "https://www.google.com/intl/ja/tagmanager/",
+    },
+    {
+      name: "Yahoo! Tag Manager",
+      id: "ytm",
+      domain: "yjtag.jp",
+      url: "https://tagmanager.yahoo.co.jp/",
+    },
+    {
+      name: "Google AdSense",
+      id: "adsense",
+      domain: "googlesyndication.com",
+      url: "https://www.google.co.jp/adsense/",
+    },
+    {
       name: "Google Analytics",
       id: "ga",
       domain: "google-analytics.com",
+      url: "https://www.google.com/intl/ja_jp/analytics/",
     },
     {
       name: "KARTE",
       id: "karte",
       domain: "karte.io",
+      url: "https://karte.io/",
     },
     {
       name: "Adobe Marketing Cloud",
       id: "adobe",
       domain: "dpm.demdex.net",
+      url: "http://www.adobe.com/jp/marketing-cloud.html",
     },
     {
       name: "SILVER EGG",
       id: "set",
       domain: "silveregg.net",
+      url: "http://www.silveregg.co.jp/",
     },
     {
       name: "Rtoaster",
       id: "rtoaster",
       domain: "rtoaster.jp",
+      url: "http://www.rtoaster.com/",
     },
     {
       name: "CombzReco",
       id: "combz",
       domain: "combz.jp",
+      url: "https://reco.combz.jp/",
     },
     {
       name: "NaviPlus",
       id: "naviplus",
       domain: "snva.jp",
+      url: "http://www.naviplus.co.jp/",
     },
     {
       name: "ec-CONCIER",
       id: "ec_concier",
       domain: "ec-concier.com",
+      url: "https://ec-concier.com/",
     },
     {
       name: "Sprocket",
       id: "sprocket",
       domain: "sprocket.bz",
+      url: "https://www.sprocket.bz/",
     },
     {
       name: "ZenClerk",
       id: "zenclerk",
       domain: "zenclerk.com",
+      url: "https://www.zenclerk.com/",
     },
     {
       name: "FLIPDESK",
       id: "flipdesk",
       domain: "flipdesk.jp",
+      url: "https://flipdesk.jp/",
     },
     {
       name: "Chamo",
       id: "chamo",
       domain: "chamo-chat.com",
+      url: "http://chamo-chat.com/",
     },
     {
       name: "Zendesk Chat",
       id: "zendesk_chat",
-      domain: "zopim",
+      domain: "zopim.com",
+      url: "https://zopimjp.com/",
     },
     {
       name: "Intercom",
       id: "intercom",
       domain: "intercomcdn.com",
+      url: "https://www.intercom.com/",
     },
     {
       name: "Usergram",
       id: "usergram",
       domain: "usergram.info",
+      url: "http://www.bebit.co.jp/usergram/",
     },
     {
       name: "SATORI",
       id: "satori",
       domain: "satori.segs.jp",
+      url: "https://satori.marketing/",
     },
   ]
 }
